0379a55d5bdb5975f04df0918b07fc07eb4a34c7
NaroMori <reply.naromori@gmail.com>
2024-04-15 08:44:27 +0300

    clear fix

--- app/App.py
+++ app/App.py
@@ -17,13 +17,12 @@ class App:
         self.O0: App.O0 = App.O0()
 
 
-    def clear():
+    def clear(self):
         
         if sys.platform.startswith('win'):
             system("cls")
         else:
             system("clear")
-
 
     def gettype():
         valid_chars: list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', '{', ']', '}', '\\', '|', ';', ':', "'", '"', ',', '<', '.', '>', '/', '?', ' ']
